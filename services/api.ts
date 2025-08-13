6c5e8babd149d6f32e45fe8790f78d872cea9cb7
harshitamahori <harshitamahori02@gmail.com>
2025-08-13 13:43:51 +0530

    Initial Folder Structure

---
+++ services/api.ts
@@ -0,0 +1,63 @@
+
+export const TMDB_CONFIG={
+    BASE_URL:'https://api.themoviedb.org/3',
+    API_KEY:process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers:{
+        accept:'application/json',
+        Authorization:`Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+export const fetchMovies=async({query}:{query:string})=>{
+    const endpoint = query?
+    `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    :`${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint,{
+        method:'GET',
+        headers:TMDB_CONFIG.headers,
+    });
+    if(!response.ok){
+        throw new Error(`Failed to fetch movies: ${response.statusText}`);
+    }
+    const data=await response.json();
+    return data.results;
+}
+
+export const fetchMovieDetails= async (movieId:string) : Promise<MovieDetails> => {
+    try{
+        const response= await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,{
+             method:'GET',
+            headers:TMDB_CONFIG.headers
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to fetch movie details');
+        }   
+       
+        const data= await response.json();
+        return data;
+        
+    }
+
+    catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+
+
+// const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMGM4MDZlZWM0MzE4Mjk1NzhiY2YyMDNjYzAxYzRkOSIsIm5iZiI6MTc1MDA4MjM1OS44MzUsInN1YiI6IjY4NTAyMzM3M2NjMTU3YTUwOTFiZjkwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eiyj6Gl_AjBo1XVI9ghjaUQSXc_GSebKzmRXodlgDoc'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
